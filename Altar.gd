89d811127d25596710241cc4f7e87c5e46d4a033
ichinii <sven.salt@yahoo.de>
2018-12-01 18:52:11 +0000

    particles ticles picles

--- Altar.gd
+++ Altar.gd
@@ -3,8 +3,12 @@ extends Node2D
 func _on_Area2D_body_entered(body):
 	if body.has_method("sacrify"):
 		body.sacrify()
+		$FinishSacrificeTimer.start()
 		$Particles2D.emitting = true
 		#todo start anim
+	if body.name == "Player":
+		$FinishSacrificeTimer.start()
+		$Particles2D.emitting = true
 
 func _on_FinishSacrificeTimer_timeout():
 	$Particles2D.emitting = false
