47a5237115b568bb1af39c958e5fa7fe159f206e
memoryleak47 <memoryleak47@protonmail.ch>
2018-12-01 16:44:50 +0100

    fixed player collision

--- Player.gd
+++ Player.gd
@@ -1,9 +1,5 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 var velocity = Vector2(0, 0)
 
 # Called when the node enters the scene tree for the first time.
@@ -15,12 +11,29 @@ func _physics_process(delta):
 	var dir = (Input.is_action_pressed("ui_right") as int) - (Input.is_action_pressed("ui_left") as int)
 	
 	# movement
-	velocity.x += dir * delta * 700
+	velocity.x += dir * delta * 1000
 	
 	# gravity
-	velocity.y += 40 * delta
+	velocity.y += 120 * delta
 	
 	# drag
 	velocity *= pow(0.9, delta)
+
+	var original_vel = velocity
+	var motion = velocity * delta
+	var c = move_and_collide(motion)
 	
-	move_and_collide(velocity * delta)
+	while c:
+		if c.remainder.length() == 0:
+			return
+		
+		var n = c.normal
+
+		velocity = velocity.slide(n)
+		motion = velocity.normalized() * c.remainder.dot(velocity.normalized())
+		
+		# check that the resulting velocity is not opposite to the original velocity, which would mean moving backward.
+		if original_vel.dot(velocity) > 0:
+			c = move_and_collide(motion)
+		else:
+			c = null
