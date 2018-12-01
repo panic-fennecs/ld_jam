56e4eaadb1cad6b950d1137e19d9386c203f1a18
memoryleak47 <memoryleak47@protonmail.ch>
2018-12-01 20:28:49 +0100

    simplified grounded system

--- Player.gd
+++ Player.gd
@@ -5,10 +5,6 @@ var can_jump = true
 var can_dash = true
 var looks_right = true
 var health = 100
-var grounded = false
-var lefted = false
-var righted = false
-var ceiled = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,6 +12,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	if is_grounded():
+		can_dash = true
+		can_jump = true
+	
 	var dir = (Input.is_action_pressed("ui_right") as int) - (Input.is_action_pressed("ui_left") as int)
 	if dir == 1:
 		looks_right = true
@@ -34,15 +34,17 @@ func _physics_process(delta):
 	
 	# jump
 	if Input.is_action_just_pressed("ui_up"):
+		print("jump!")
+		print(is_grounded())
+		print("==")
 		var will_jump = false
-		if grounded:
+		if is_grounded():
 			will_jump = true
 		elif can_jump:
 			will_jump = true
 			can_jump = false
 		if will_jump:
 			velocity.y = -500
-			grounded = false
 	
 	if Input.is_action_just_pressed("dash") and can_dash:
 		var dash_dir = (looks_right as int) * 2 - 1
@@ -53,28 +55,11 @@ func _physics_process(delta):
 	var motion = velocity * delta
 	var c = move_and_collide(motion)
 
-	grounded = false
-	lefted = false
-	righted = false
-	ceiled = false
-
-	var did_collide_ground = false
-	var did_collide_left = false
-	var did_collide_right = false
-	var did_collide_ceil = false
 	while c:
 		if c.remainder.length() == 0:
 			break
 		
 		var n = c.normal
-		if (n - Vector2(0, -1)).length() < 0.01:
-			did_collide_ground = true
-		if (n - Vector2(1, 0)).length() < 0.01:
-			did_collide_left = true
-		if (n - Vector2(-1, 0)).length() < 0.01:
-			did_collide_right = true
-		if (n - Vector2(0, 1)).length() < 0.01:
-			did_collide_ceil = true
 
 		velocity = velocity.slide(n)
 		motion = velocity.normalized() * c.remainder.dot(velocity.normalized())
@@ -84,19 +69,12 @@ func _physics_process(delta):
 			c = move_and_collide(motion)
 		else:
 			c = null
-	if did_collide_ground and !grounded:
-		now_grounded()
-	if did_collide_left and !lefted:
-		lefted = true
-	if did_collide_right and !righted:
-		righted = true
-	if did_collide_ceil and !ceiled:
-		ceiled = true
 
-func now_grounded():
-	grounded = true
-	can_jump = true
-	can_dash = true
+func collides_direction(x):
+	return test_move(transform, x)
+
+func is_grounded():
+	return collides_direction(Vector2(0, 1))
 
 func die():
 	print("you are dead. Too bad.")
