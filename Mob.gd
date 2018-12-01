fded6712f893e039c67d9c2e28207354ade2f6e9
Bluemi <bruno.schilling@protonmail.ch>
2018-12-01 19:04:28 +0100

    Implemented Mob Movement

--- Mob.gd
+++ Mob.gd
@@ -12,13 +12,15 @@ const ACCELERATION = 10;
 
 func _process_moving():
 	if target_velocity > 0:
-		if not _right_sensor_colliding():
+		if not _right_bot_sensor_colliding():
 			target_velocity = -MAX_SPEED
-			print("left")
+		if _right_sensor_colliding():
+			target_velocity = -MAX_SPEED
 	elif target_velocity < 0:
-		if not _left_sensor_colliding():
+		if not _left_bot_sensor_colliding():
 			target_velocity = MAX_SPEED
-			print("right")
+		if _left_sensor_colliding():
+			target_velocity = MAX_SPEED
 	else:
 		target_velocity = MAX_SPEED
 
@@ -35,11 +37,6 @@ func _calculate_state():
 func _physics_process(delta):
 	print("-")
 	_calculate_state()
-	
-	print("left_colliding:")
-	print(_left_sensor_colliding())
-	print("right_colliding:")
-	print(_right_sensor_colliding())
 
 	if state == State["MOVING"]:
 		_process_moving()
@@ -48,21 +45,49 @@ func _physics_process(delta):
 
 	_adjust_velocity()
 
-	move_and_slide(velocity)
+	_apply_velocity(delta)
+
+func _apply_velocity(delta):
+	var original_vel = velocity
+	var motion = velocity * delta
+	var c = move_and_collide(motion)
+
+	while c:
+		if c.remainder.length() == 0:
+			return
+
+		var n = c.normal
+
+		velocity = velocity.slide(n)
+		motion = velocity.normalized() * c.remainder.dot(velocity.normalized())
+
+		# check that the resulting velocity is not opposite to the original velocity, which would mean moving backward.
+		if original_vel.dot(velocity) > 0:
+			c = move_and_collide(motion)
+		else:
+			c = null
 
 func _turn():
 	target_velocity = -target_velocity
 
-func _right_sensor_colliding():
-	var bodies = $SensorRight.get_overlapping_bodies()
+func _right_bot_sensor_colliding():
+	var bodies = $SensorRightBot.get_overlapping_bodies()
+	return bodies.size() != 0
+
+func _left_bot_sensor_colliding():
+	var bodies = $SensorLeftBot.get_overlapping_bodies()
+	return bodies.size() != 0
+	
+func _bot_sensor_colliding():
+	var bodies = $SensorBot.get_overlapping_bodies()
 	return bodies.size() != 0
 
 func _left_sensor_colliding():
 	var bodies = $SensorLeft.get_overlapping_bodies()
 	return bodies.size() != 0
-	
-func _bot_sensor_colliding():
-	var bodies = $SensorBot.get_overlapping_bodies()
+
+func _right_sensor_colliding():
+	var bodies = $SensorRight.get_overlapping_bodies()
 	return bodies.size() != 0
 
 func _adjust_velocity():
