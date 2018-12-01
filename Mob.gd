7ebeaab6da0d59aa6c8e3002863a7eec49de3bb8
Bluemi <bruno.schilling@protonmail.ch>
2018-12-01 15:47:06 +0100

    implemented basic Mob

--- Mob.gd
+++ Mob.gd
@@ -4,5 +4,16 @@ var velocity = Vector2(0, 0);
 var direction = 1;
 var step = 0;
 
+const MAX_SPEED = 100;
+const ACCELERATION = 10;
+
 func _physics_process(delta):
-	pass
+	if step == 100:
+		direction = -direction
+		step=0
+	step+=1
+	velocity.x += direction * ACCELERATION
+	velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+	velocity.y = 50;
+	move_and_slide(velocity)
+	# TODO redo velocity.y
