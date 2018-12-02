d1e4aad5cc4aa0c4bb66c33d4b27eaee3072f127
Philipp <philippboensch@gmail.com>
2018-12-02 15:16:43 +0100

    initial move animation

--- Player.gd
+++ Player.gd
@@ -2,6 +2,11 @@ extends KinematicBody2D
 
 const MAX_DASH_COUNTER = 0.03
 
+enum DIRECTION {
+	left = -1,
+	right = 1
+}
+
 var velocity = Vector2(0, 0)
 var can_jump = true
 var can_dash = true
@@ -9,18 +14,21 @@ var looks_right = true
 var health = 100
 var dash_counter = 0
 var carry = null
+var look_dir = DIRECTION.right
+var last_look_dir = DIRECTION.right 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	update_healthbar()
+	$AnimationPlayer.play("Move")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var dir = (Input.is_action_pressed("ui_right") as int) - (Input.is_action_pressed("ui_left") as int)
 	if dir == 1:
-		looks_right = true
+		look_dir = DIRECTION.right
 	elif dir == -1:
-		looks_right = false
+		look_dir = DIRECTION.left
 	
 	if dash_counter == 0:
 		# movement
@@ -48,7 +56,12 @@ func _physics_process(delta):
 	if is_grounded():
 		can_dash = true
 		can_jump = true
-
+	
+	if last_look_dir != look_dir:
+		$CharacterSprite.scale.x *= -1
+	
+	last_look_dir = look_dir
+	
 func jump():
 	velocity.y = -1500
 
@@ -63,7 +76,7 @@ func try_jump():
 func try_dash():
 	if Input.is_action_just_pressed("dash") and can_dash:
 		dash_counter = MAX_DASH_COUNTER
-		var dash_dir = (looks_right as int) * 2 - 1
+		var dash_dir = (look_dir as int) * 2 - 1
 		velocity.x = dash_dir * 2000
 		velocity.y = 0
 		can_dash = false
