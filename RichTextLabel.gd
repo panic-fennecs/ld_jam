55d4a8a20bfa24fed338f6ca3a3ed427e1ebf6a4
c44p <phandoom8@gmail.com>
2018-12-04 03:49:31 +0100

    added credits

---
+++ RichTextLabel.gd
@@ -0,0 +1,20 @@
+extends RichTextLabel
+
+
+func _ready():
+	self.append_bbcode("[center] Made by [/center] \n\n\n\n")
+	self.append_bbcode("[center] OGtoad [/center] \n\n")
+	self.append_bbcode("[center] Spix [/center] \n\n")
+	self.append_bbcode("[center] memoryleak47 [/center] \n\n")
+	self.append_bbcode("[center] philippkick [/center] \n\n")
+	self.append_bbcode("[center] alok [/center] \n\n")
+	self.append_bbcode("[center] cap [/center] \n\n\n\n\n")
+	
+	self.append_bbcode("[center] special thanks to [/center] \n\n\n\n")
+	self.append_bbcode("[center] the Godot - Community on discord [/center] \n\n")
+	self.append_bbcode("[center] for letting us know that \n the dev version of Godot is broken[/center] \n\n\n\n")
+	self.append_bbcode("[center] AMRPHIS [/center] \n\n")
+	self.append_bbcode("[center] for the demonic background music [/center] \n")
+	self.append_bbcode("[center] just check him out on youtube [/center] \n")
+	self.append_bbcode("[center] /user/AMRPHIS/ [/center] \n\n")
+
