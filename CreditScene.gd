3e04fdc3d7185f8283225056dbdd3e2f233b2d0a
ichinii <sven.salt@yahoo.de>
2018-12-04 03:19:39 +0000

    fixed credit scene

--- CreditScene.gd
+++ CreditScene.gd
@@ -4,7 +4,7 @@ func _ready():
 	$CharacterSprite/AnimationPlayer.play("EndScreen")
 
 func _process(delta):
-	$TextNode.position.y -= delta * 50
+	$TextNode.position.y -= delta * 100
 
 func _on_Timer_timeout():
-	get_tree().change_scene("res://Start.tscn")
+	get_tree().change_scene("res://StartScene.tscn")
--- Main.gd
+++ Main.gd
@@ -5,7 +5,6 @@ export (PackedScene) var audioplayer
 const LEVEL0 = preload("res://level_0.tscn")
 const LEVEL1 = preload("res://Terrain.tscn")
 const LEVEL2 = preload("res://level_2.tscn")
-const ENDSCREEN = preload("res://EndScene.tscn")
 
 func _ready():
 	var audioplayerInstance = audioplayer.instance()
@@ -21,5 +20,4 @@ func _ready():
 		var level = LEVEL2.instance()
 		add_child(level)
 	elif GlobalState.level == 3:
-		var end_screen = ENDSCREEN.instance()
-		add_child(end_screen)
+		get_tree().change_scene("res://EndScene.tscn")
